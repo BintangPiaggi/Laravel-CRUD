eee3c7de76380f4ed9b156535debc8cc140afa92
Bintang Piaggi Putra <bintang.piaggi44@gmail.com>
2019-04-30 06:19:26 +0700

    first commit

---
+++ database/migrations/2019_04_23_013437_create_bukus_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateBukusTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('bukus', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('judul', 30);
+            $table->string('pengarang', 30);
+            $table->tinyInteger('tahun_terbit');
+            $table->string('penerbit', 30);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('bukus');
+    }
+}
